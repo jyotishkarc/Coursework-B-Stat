26762a1d9707e8c0a3b4436c72f331167a1ab5a8
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2022-05-17 23:13:51 +0530

    QiVj for different i and j

--- B.Stat 3rd year/Statistics Comprehensive/Group Project/HHV1.R
+++ B.Stat 3rd year/Statistics Comprehensive/Group Project/HHV1.R
@@ -4,25 +4,51 @@ path <- "D:/My Documents/PLFS-data/FINAL_DATA_2019-20/TEXT/"
 path.files <- list.files(path)
 
 HHV1 <- read.csv(paste0(path,path.files[2]), header = FALSE)
+HHRV <- read.csv(paste0(path,path.files[1]), header = FALSE)
 
-temp <- HHV1 %>%
-         apply(1, function(val){
-                     return(val %>% strsplit(split = "") %>% unlist())
-               }) %>% t()
+temp.v1 <- HHV1 %>%
+            apply(1, function(val){
+               return(val %>% strsplit(split = "") %>% unlist())
+            }) %>% t()
 
-colnames(temp) <- col1 <- c(1:ncol(temp)) %>% 
-                              sapply(function(val) paste0("V",val))
+temp.rv <- HHRV %>%
+            apply(1, function(val){
+               return(val %>% strsplit(split = "") %>% unlist())
+            }) %>% t()
 
-temp.urban <- temp %>% as.data.frame() %>% filter(V12 == 2)
+colnames(temp.v1) <- colnames(temp.rv) <- c(1:ncol(temp.v1)) %>% 
+                                             sapply(function(val) paste0("V",val))
 
-hce <- c()
-for (i in 1:nrow(temp.urban)) {
-   hce[i] <- paste0(temp.urban[i, 48:55]) %>% 
-               stringr::str_c(collapse = "") %>% 
-               as.numeric()
+temp.urban.v1 <- temp.v1 %>% as.data.frame() %>% filter(V12 == 2)
+temp.urban.rv <- temp.rv %>% as.data.frame() %>% filter(V12 == 2)
+
+temp.qivj <- hce <- list()
+
+for(i in 1:4) {
+   hce[[i]] <- temp.qivj[[i]] <- list()
+   
+   for(j in 1:4){
+      if(j == 1){doc <- temp.urban.v1}
+      if(j > 1){doc <- temp.urban.rv}
+      
+      hce[[i]][[j]] <- rep(0, 86)
+      
+      temp.qivj[[i]][[j]] <- doc %>% as.data.frame() %>% 
+                                 filter(V9 == i & V11 == j)
+      
+      B <- temp.qivj[[i]][[j]]
+      
+      for(k in 1:nrow(B)){
+         hce[[i]][[j]][k] <- B[k, 48:55] %>% paste0() %>% 
+                                 stringr::str_c(collapse = "") %>% 
+                                 as.numeric()
+      }
+      
+      print(j)
+   }
 }
 
-hce.sig <- hce[hce > 5 & hce < 130500]
+# hce.sig <- hce[hce > 5 & hce < 130500]
 
 
 ################
@@ -30,7 +56,7 @@ hce.sig <- hce[hce > 5 & hce < 130500]
 # ks.test(rnorm(1000,mean = 1, sd = 1)-1,"pnorm")
 
 #### Box-Cox Transformation
-if(FALSE){
+if(TRUE){
    G <- boxcox(hce.sig ~ 1)
    lmb <- G$x[which.max(G$y)]
    tr.hce <- (hce.sig^lmb - 1)/lmb
@@ -45,7 +71,9 @@ if(TRUE){
    M <- param$estimate[1]
    S <- param$estimate[2]
    
-   ks.test((tr.hce + rnorm(length(tr.hce), 1, sd = sqrt(S^2/10))-M-1)/sqrt(S^2+S^2/10),
+   ks.test((tr.hce + rnorm(length(tr.hce), 
+                           mean = 1, 
+                           sd = sqrt(S^2/100))-M-1)/sqrt(S^2+S^2/100), 
            "pnorm")
 }
 
